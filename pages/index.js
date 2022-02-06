fb05cf97c3447362b9a1e599129334987325ba37
Chris Hannaby <chris.hannaby@netlify.com>
2022-02-05 22:40:28 -0700

    fetch some data server side

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
 import db from '../db.json';
+import dynamoDb from '../lib/dynamo-db';
 
-export default function App() {
-  return <p>{db.TableName}</p>;
+export async function getServerSideProps() {
+  const params = {
+    TableName: db.TableName,
+    KeyConditionExpression: '#yr = :yyyy',
+    ExpressionAttributeNames: {
+      '#yr': 'year'
+    },
+    ExpressionAttributeValues: {
+      ':yyyy': 1985
+    }
+  };
+  const { Items } = await dynamoDb.query(params);
+  return {
+    props: {
+      movies: Items
+    }
+  };
+}
+
+export default function App({ movies }) {
+  const movieRows = movies.map((movie) => (
+    <p key={movie.title}>{movie.title}</p>
+  ));
+  return <main>{movieRows}</main>;
 }
